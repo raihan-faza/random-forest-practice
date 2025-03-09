505e100ad0804d4b758ca670e0d81a734b7426f4
lahh <raihanfazasabiqr@gmail.com>
2025-03-09 11:50:04 +0700

    adding some preprocessing function

---
+++ data_process.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_dataset() -> pd.DataFrame:
+    df = pd.read_csv("pcos_dataset.csv")
+    return df
+
+
+def split_dataset(df: pd.DataFrame, test_size: float, random_state: int) -> Tuple:
+    xtrain, xtest, ytrain, ytest = train_test_split(
+        df, test_size=test_size, random_state=random_state
+    )
+    return xtrain, xtest, ytrain, ytest
+
+
+def encode_data():
+    return
+
+
+def explore_dataset():
+    return
